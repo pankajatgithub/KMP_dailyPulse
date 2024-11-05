6c3e0d85ae1b7eada73960ebc5ad7ce92cb372ba
Pankaj <pankaj.awasthi@novoinvent.com>
2024-11-05 18:58:47 +0530

    IOS SwiftUI update

---
+++ iosApp/iosApp/AboutScreen.swift
@@ -0,0 +1,22 @@
+//
+//  AboutScreen.swift
+//  iosApp
+//
+//  Created by Pankaj Awasthi on 05/11/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutScreen: View {
+    var body: some View {
+        NavigationStack{
+            AboutListView()
+                .navigationTitle("About Device")
+        }
+    }
+}
+
+#Preview {
+    AboutScreen()
+}
---
+++ iosApp/iosApp/Untitled.swift
@@ -0,0 +1,8 @@
+//
+//  Untitled.swift
+//  iosApp
+//
+//  Created by Pankaj Awasthi on 05/11/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
